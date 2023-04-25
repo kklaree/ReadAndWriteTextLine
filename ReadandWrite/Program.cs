5e20a11892b35c0a61122c5dfd39d9e8e3d6bb96
PC <PC@DEGHIE>
2023-04-26 00:56:07 +0800

    Add project files.

---
+++ ReadandWrite/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ReadandWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            StreamReader sr = new StreamReader("C:\\Users\\PC\\Documents\\hatdog.txt");
+
+            line = sr.ReadLine();
+
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter("C:\\Users\\PC\\Documents\\hatdog.txt");
+
+            sw.WriteLine("Hello World!");
+            sw.WriteLine("From me");
+
+            sw.Close();
+        }
+    }
+}
